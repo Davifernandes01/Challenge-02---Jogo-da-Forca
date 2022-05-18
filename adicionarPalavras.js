480fb091e9288fac76b39a5b8e67d1e5379f9938
davi <davifernadesr@gmail.com>
2022-05-17 21:22:19 -0300

    primeiro commit

---
+++ adicionarPalavras.js
@@ -0,0 +1,16 @@
+
+let btnAdicionar = document.querySelector(".btn-adicionar");
+let salvarPalavras = []
+let input = document.querySelector(".input")
+
+btnAdicionar.addEventListener("click",function()
+{
+
+     
+      let adiciona = salvarPalavras.push(input.value);
+     console.log(salvarPalavras);
+     
+     
+     
+})
+
