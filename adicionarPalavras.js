362fb1552bedf25d7837a7b66a46f9c9f7fc6916
davi <davifernadesr@gmail.com>
2022-05-19 21:41:35 -0300

    agora o  usuario nao podera usar letras minusculas, com acentos e nem numeros para digitsr, apenas letras maiusculas e sem acento

--- adicionarPalavras.js
+++ adicionarPalavras.js
@@ -1,14 +1,16 @@
 
 let btnAdicionar = document.querySelector(".btn-adicionar");
 let salvarPalavras = ["CAMELO", "AMEIXA","CROACIA"]
-localStorage.setItem("salvarPalavras", JSON.stringify(salvarPalavras))
+
 let input = document.querySelector(".input")
 
 btnAdicionar.addEventListener("click",function()
 {
- 
-      let adiciona = salvarPalavras.push(input.value);
-     console.log(salvarPalavras); 
    
+       let adiciona = salvarPalavras.push(input.value);
+             console.log(salvarPalavras);      
+            localStorage.setItem("salvarPalavras", JSON.stringify(salvarPalavras)) 
+            
 })
+
 
--- jogoForca.js
+++ jogoForca.js
@@ -1,2 +1,27 @@
-let adicionaSalvarPalavras = JSON.parse(localStorage.getItem("salvarPalavras"));
-console.log(adicionaSalvarPalavras);
+//pega o array da pagina adicionarPalavras e passa para pagina de jogoForca
+let adicionaSalvarPalavras = JSON.parse(localStorage.getItem("salvarPalavras")); 
+//console.log(adicionaSalvarPalavras);
+
+//escolhendo a palavra sorteada, por meio de um sorteio
+
+let palavraSecreta = adicionaSalvarPalavras[Math.floor(Math.random() * adicionaSalvarPalavras.length )]
+
+console.log(palavraSecreta);
+
+// evento para saber se o usuario clicou em uma letr
+document.addEventListener("keydown" , (evento)=>
+    {
+        let codigo = evento.keyCode; //65 - 90
+
+        if(letraTrue(codigo))
+        {
+            let letra = evento.key;
+            console.log(letra);
+        }
+    } )
+
+
+            function letraTrue(codigo)
+        {
+            return codigo >= 65 &&  codigo <= 90;
+        }
