c49777f22f07c4406b8626b02e5017d867c0b6bb
davi <davifernadesr@gmail.com>
2022-05-28 14:05:23 -0300

    adicionando as funçoes para que o usario aperte somente as letras maiusculas e sem acento

--- jogoForca.js
+++ jogoForca.js
@@ -1,14 +1,12 @@
 //pega o array da pagina adicionarPalavras e passa para pagina de jogoForca
-let adicionaSalvarPalavras = JSON.parse(localStorage.getItem("salvarPalavras")); 
-//console.log(adicionaSalvarPalavras);
+let adicionaSalvarPalavras = JSON.parse(localStorage.getItem("salvar-palavras"));
+//console.log(adicionaSalvarPalavras)
 
 //escolhendo a palavra sorteada, por meio de um sorteio
-
 let palavraSecreta = adicionaSalvarPalavras[Math.floor(Math.random() * adicionaSalvarPalavras.length )]
-
 console.log(palavraSecreta);
 
-// evento para saber se o usuario clicou em uma letr
+// evento para saber se o usuario clicou em uma letra
 document.addEventListener("keydown" , (evento)=>
     {
         let codigo = evento.keyCode; //65 - 90
